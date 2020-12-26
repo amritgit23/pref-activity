552a0e2c64ebda79fce1080d215340b94125a1e0
amritgit23 <46225948+amritgit23@users.noreply.github.com>
2020-12-26 18:08:11 -0500

    Add files via upload

--- activity.js
+++ activity.js
@@ -1,78 +1,11 @@
-npm install amd-loader;
-require("amd-loader");
-define([
-    'postmonger'
-], function (
-    Postmonger
-) {
-    'use strict';
-    var connection = new Postmonger.Session();
-    var token;
-    var payload = {};
-    var contactKey;
+var connection = new Postmonger.Session();
 
-    $(window).ready(function() {
-        connection.trigger('requestTokens');
-        connection.trigger('ready');
-    });
+connection.trigger('ready');
+connection.on('initActivity',function(data){
+ document.getElementByID('configuration').value = JSON.stringify(data,null,2);
+});
 
-    connection.on('clickedNext', save);
-
-    connection.on('getTokens', function( data ) {
-        if( data.error ) {
-            console.error( data.error );
-        } else {
-            tokens = data;
-        }
-    });
-    
-    connection.on('initActivity', function(payload) { 
-        console.log("INITACTIVITY INITACTIVITY INITACTIVITY INITACTIVITY INITACTIVITY ");
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-
-        var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-
-        console.log("INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS ");
-        console.log(inArguments);
-        console.log("INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS INARGS ");
-
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-                if (key === 'contactKey') {
-                    contactKey = val;
-                }            
-            });
-        });
-        console.log(payload);
-
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
-        console.log("INITACTIVITY INITACTIVITY INITACTIVITY INITACTIVITY INITACTIVITY ");
-     });
-
-
-
-    connection.on('requestedTokens', function(tokens) { 
-        token = tokens;
-    });
-
-       function save() {
-        payload['arguments'].execute.inArguments = [{
-            "tokens": token,
-            "contactKey": contactKey
-        }];
-        
-        payload['metaData'].isConfigured = true;
-
-        console.log(payload);
-        connection.trigger('updateActivity', payload);
-    }
+connection.on('clickedNext',function(){
+ var configuration = JSON.parse(document.getElementByID('configuration').value);
+ connection.trigger('updateActivity',configuration);
 });
