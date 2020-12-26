bc4aaf97b1fcea199161e822dc1950768a7c3104
amritgit23 <46225948+amritgit23@users.noreply.github.com>
2020-12-26 16:13:11 -0500

    Add files via upload

---
+++ activity.js
@@ -0,0 +1,11 @@
+﻿var connection = new Postmonger.Session()
+
+connection.trigger('ready');
+connection.on('initActivity',function(data){
+  document.getElementByID('configuration').value = JSON.stringify(data,null,2);
+});
+
+connection.on('clickedNext',function(){
+  var configuration = JSON.parse(document.getElementByID('configuration').value);
+  connection.trigger('updateActivity',configuration);
+});
